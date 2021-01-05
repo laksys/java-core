bb19cf73a868cef6a553ffb8c94d671ef4e759a7
lakshmanan <laksys@gmail.com>
2021-01-05 19:12:09 +0530

    first commit

---
+++ second/src/com/king/Welcome.java
@@ -0,0 +1,10 @@
+package com.king;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test project!");
+	}
+
+}
